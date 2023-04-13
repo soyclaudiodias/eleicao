bcdac42716d60d6be11e92d82b01eb902fbe5f85
Gustavo Scalabrini Sampaio <scalabrinig@gmail.com>
2023-04-13 19:10:37 -0300

    Falhar

--- src/main/java/com/mackenzie/eleicao/Eleitor.java
+++ src/main/java/com/mackenzie/eleicao/Eleitor.java
@@ -15,7 +15,7 @@ public class Eleitor {
         
         String mtOutClassificacao;
               
-        if (mtInIdade >= 18 && mtInIdade < 70){
+        if (mtInIdade >= 0 && mtInIdade < 70){
             mtOutClassificacao = "Obrigatorio";
         }
         else if (mtInIdade >= 0 && mtInIdade <= 15){
