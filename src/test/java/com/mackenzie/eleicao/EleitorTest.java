c0ed20aaba3e464b8a89659a4d0cdf3094e08f9f
Cláudio Dias <113221142+soyclaudiodias@users.noreply.github.com>
2025-03-24 10:56:06 -0300

    Update EleitorTest.java

--- src/test/java/com/mackenzie/eleicao/EleitorTest.java
+++ src/test/java/com/mackenzie/eleicao/EleitorTest.java
@@ -22,7 +22,7 @@ public class EleitorTest {
     @Test
     public void testMtClasssificarEleitorInvalido() {
         System.out.println("Teste da classe Idade Invalida");
-        assertEquals("Idade Invalida", objEleitor.mtClasssificarEleitor(-1));
+        assertEquals("Idade Invalida", objEleitor.mtClasssificarEleitor(0));
         assertEquals("Idade Invalida", objEleitor.mtClasssificarEleitor(131));
     }
     
